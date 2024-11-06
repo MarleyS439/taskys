8194e0e8bd02ef9672fc826eee32d3b7a72da597
MarleyS439 <marleysantos439@gmail.com>
2024-11-06 20:47:00 -0300

    Troca de Bulma CSS por CSS puro e Tela login Desktop

--- README.md
+++ README.md
@@ -3,7 +3,18 @@
 
 ## Software de gerenciamento de tarefas
 
-- Flexível
-- Simples
-- Eficiente
-- Poderoso
+### Flexível 
+
+Adaptável a diferentes usos, desde um simples espaço de acompanhamento de tarefas pessoais a gestão de tarefas delegadas
+
+### Simples
+
+Com um design minimalista, livre de distrações
+
+### Eficiente 
+
+Registrar e acompanhar tarefas nunca foi tão fácil
+
+### Colaborativo
+
+Registre, delegue e acompanhe tarefas com a sua equipe
--- client/view/pages/login.php
+++ client/view/pages/login.php
@@ -1,11 +1,67 @@
 <!DOCTYPE html>
 <html lang="pt-br" dir="ltr">
+
 <head>
+    <!-- HTML Meta Tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Marley de S. Santos">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="../assets/css/login.css">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="../assets/icons/favicon.ico" type="image/x-icon">
+
+    <!-- Title -->
     <title>Login</title>
 </head>
+
 <body>
-    <h1>Login</h1>
+    <div class="container">
+        <div class="image">
+            <img src="../assets/images/people.png" alt="">
+        </div>
+
+        <div class="login">
+            <div class="logotipo">
+                <img src="../assets/images/banner.png" alt="Logotipo Tasky">
+            </div>
+
+            <div class="box-login">
+                <form action="" method="post">
+                    <div class="title">
+                        <h2>Login</h2>
+                    </div>
+
+                    <div class="inputs">
+                        <label for="email">E-mail</label>
+                        <input type="email" name="email" id="email" placeholder="seu_email@email.com" required>
+                    </div>
+
+                    <div class="inputs">
+                        <label for="senha">Senha</label>
+                        <input type="password" name="senha" id="senha" placeholder="Digite sua senha" required>
+                    </div>
+
+                    <div class="inputs">
+                        <button type="submit">Entrar</button>
+                    </div>
+
+                    <div class="forgot">
+                        <a href="#">Esqueci minha senha</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Copyright -->
+    <div class="copy">
+        <p>&copy; 2024. Todos os direitos reservados. <a href="https://github.com/MarleyS439" target="_blank">Marley de S. Santos</a></p>
+    </div>
 </body>
+
 </html>
